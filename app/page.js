fe499807a3710dfe86f45184f21fee33fd440dc7
Kris Kuchinka <kriskuchinka@gmail.com>
2023-06-19 11:05:13 -0700

    more changes

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+'use client'
 import Image from 'next/image'
 import styles from './page.module.css'
 import Hero1 from './components/Hero1'
@@ -10,19 +11,22 @@ import Hero4 from './components/Hero4'
 import MainContent4 from './components/MainContent4'
 import Hero5 from './components/Hero5'
 
+import { ParallaxProvider } from 'react-scroll-parallax'
 
 export default function Home() {
   return (
     <>
-    <Hero1 />
-    <MainContent1 />
-    <Hero2 />
-    <MainContent2 />
-    <Hero3 />
-    <MainContent3 />
-    <Hero4 />
-    <MainContent4 />
-    <Hero5 />
+    <ParallaxProvider>
+      <Hero1 />
+      <MainContent1 />
+      <Hero2 />
+      <MainContent2 />
+      <Hero3 />
+      <MainContent3 />
+      <Hero4 />
+      <MainContent4 />
+      <Hero5 />
+    </ParallaxProvider>
     </>
   )
 }
